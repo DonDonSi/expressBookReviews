4c30688ad95a690d3a7918c75632067695b4f567
DonDonSi <162065255+DonDonSi@users.noreply.github.com>
2024-09-16 15:44:04 -0600

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 
 public_users.post("/register", (req,res) => {
@@ -58,4 +59,77 @@ public_users.get('/review/:isbn',function (req, res) {
   return res.status(200).json({ reviews });
 });
 
+// Tasks 10-13
+
+const exampleUrl= "http://localhost:5000";
+
+// Task 10
+public_users.get('/async-get-books', async function (req, res) {
+    try {
+        await new Promise(resolve => setTimeout(resolve, 100)); 
+    res.send(JSON.stringify({ books }, null, 4));
+  } catch (error) {
+    res.status(500).json({ message: "Error getting books" });
+  }
+});
+
+// Task 11
+public_users.get('/async-isbn/:isbn', function (req, res) {
+  const { isbn } = req.params;
+ axios.get(`${exampleUrl}/${isbn}`)
+    .then(response => {
+      const book = response.data;
+
+      if (book) {
+        res.status(200).json(book);
+      } else {
+        res.status(404).json({ message: "Book not found" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Error getting book" });
+    });
+});
+
+
+// Task 12
+public_users.get('/async-author/:author', async function (req, res) {
+    const { author } = req.params;
+
+    try {
+      await new Promise(resolve => setTimeout(resolve, 100)); 
+      
+      const filteredBooks = Object.values(books).filter(book => book.author === author);
+  
+      if (filteredBooks.length > 0) {
+        res.status(200).send(JSON.stringify({ filteredBooks }, null, 4));
+      } else {
+        res.status(404).json({ message: "Books not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Error getting books" });
+    }
+  });
+
+// Task 13
+public_users.get('/async-title/:title', async function (req, res) {
+
+    const { title } = req.params;
+
+    try {
+      await new Promise(resolve => setTimeout(resolve, 100)); // Simulate async operation
+      
+      const filteredBooks = Object.values(books).filter(book => book.title === title);
+  
+      if (filteredBooks.length > 0) {
+        res.status(200).send(JSON.stringify({ filteredBooks }, null, 4));
+      } else {
+        res.status(404).json({ message: "Book not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Error getting book" });
+    }
+});
+
 module.exports.general = public_users;
+
